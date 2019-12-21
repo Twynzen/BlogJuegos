6d515215e56c35cd8e0c586557db4812c18d0010
Daniel Castiblanco <razzziel001@gmail.com>
2019-12-21 17:35:03 -0500

    Cree rutas entre secciones y categorias del sitio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AcercadeComponent } from './pages/acercade/acercade.component';
+import { ResenaComponent } from './pages/resena/resena.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  //así se redirige el componente 
+  {path: '', component: PortafolioComponent },
+  {path: 'acercade', component: AcercadeComponent },
+  {path: 'resena', component: ResenaComponent},
+  {path: '**', pathMatch: 'full', redirectTo: ''}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
